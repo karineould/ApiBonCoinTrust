15f61b2bbae5012cc72acf9c6ef1f329a5723b81
Karine Ouldbraham <k.ouldbraham@weezevent.com>
2018-02-16 11:31:24 +0100

    create controller avis

---
+++ api/controllers/avisController.js
@@ -0,0 +1,30 @@
+var Avis   = require('../models/avis');
+
+
+exports.findAll = function(req, res) {
+
+};
+
+
+exports.findAllByClient = function(req, res) {
+
+};
+
+
+exports.findAllByAnnonce = function(req, res) {
+
+};
+
+exports.findOneByClient = function(req, res) {
+
+};
+
+
+exports.findoneByAnnonce = function(req, res) {
+
+};
+
+
+exports.createAvis = function(req, res) {
+
+};
